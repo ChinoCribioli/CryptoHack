95e69c82a43a4bb55eeffd660aeffc9e60fc18d7
ChinoCribioli <ezequiel.cribioli@gmail.com>
2024-10-15 21:16:08 -0300

    solved curveball

--- Elliptic_Curves/ecc.py
+++ Elliptic_Curves/ecc.py
@@ -417,3 +417,117 @@ assert(decrypt_flag(shared_secret, output['iv'], output['encrypted_flag']) == 'c
 
 ###################################################################
 
+
+#	SOURCE CODE:
+
+#!/usr/bin/env python3
+
+import fastecdsa
+from fastecdsa.point import Point
+# from utils import listener
+
+
+FLAG = "crypto{????????????????????????????????????}"
+G = fastecdsa.curve.P256.G
+assert G.x, G.y == [0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296,
+                    0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5]
+
+
+class Challenge():
+    def __init__(self):
+        self.before_input = "Welcome to my secure search engine backed by trusted certificate library!\n"
+        self.trusted_certs = {
+            'www.cryptohack.org': {
+                "public_key": Point(0xE9E4EBA2737E19663E993CF62DFBA4AF71C703ACA0A01CB003845178A51B859D, 0x179DF068FC5C380641DB2661121E568BB24BF13DE8A8968EF3D98CCF84DAF4A9),
+                "curve": "secp256r1",
+                "generator": [G.x, G.y]
+            },
+            'www.bing.com': {
+                "public_key": Point(0x3B827FF5E8EA151E6E51F8D0ABF08D90F571914A595891F9998A5BD49DFA3531, 0xAB61705C502CA0F7AA127DEC096B2BBDC9BD3B4281808B3740C320810888592A),
+                "curve": "secp256r1",
+                "generator": [G.x, G.y]
+            },
+            'www.gchq.gov.uk': {
+                "public_key": Point(0xDEDFC883FEEA09DE903ECCB03C756B382B2302FFA296B03E23EEDF94B9F5AF94, 0x15CEBDD07F7584DBC7B3F4DEBBA0C13ECD2D2D8B750CBF97438AF7357CEA953D),
+                "curve": "secp256r1",
+                "generator": [G.x, G.y]
+            }
+        }
+
+    def search_trusted(self, Q):
+        for host, cert in self.trusted_certs.items():
+            if Q == cert['public_key']:
+                return True, host
+        return False, None
+
+    def sign_point(self, g, d):
+        return g * d
+
+    def connection_host(self, packet):
+        d = packet['private_key']
+        if abs(d) == 1:
+            return "Private key is insecure, certificate rejected."
+        packet_host = packet['host']
+        curve = packet['curve']
+        g = Point(*packet['generator'])
+        Q = self.sign_point(g, d)
+        cached, host = self.search_trusted(Q)
+        if cached:
+            return host
+        else:
+            self.trusted_certs[packet_host] = {
+                "public_key": Q,
+                "curve": "secp256r1",
+                "generator": G
+            }
+            return "Site added to trusted connections"
+
+    def bing_it(self, s):
+        return f"Hey bing! Tell me about {s}"
+
+    #
+    # This challenge function is called on your input, which must be JSON
+    # encoded
+    #
+    def challenge(self, your_input):
+        host = self.connection_host(your_input)
+        if host == "www.bing.com":
+            return self.bing_it(FLAG)
+        else:
+            return self.bing_it(host)
+
+
+import builtins; builtins.Challenge = Challenge # hack to enable challenge to be run locally, see https://cryptohack.org/faq/#listener
+# listener.start_server(port=13382)
+
+#	SOLUTION
+
+import socket
+import json
+HOST = "socket.cryptohack.org"
+PORT = 13382
+
+order = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551 # order of the curve secp256r1
+# Note that, in my packet in method connection_host, I send a private key candidate but I also send my own point generator.
+# Thus, the challenge is not to find an x such that g^x = pk, but to find an x AND a base point b such that b^x = pk.
+# Taking b = pk^((order+1)/2) and x = 2 works since b^(order+1) = b for any b.
+target_pk = Point(0x3B827FF5E8EA151E6E51F8D0ABF08D90F571914A595891F9998A5BD49DFA3531, 0xAB61705C502CA0F7AA127DEC096B2BBDC9BD3B4281808B3740C320810888592A)
+bait = target_pk * ((order+1)//2)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # print(s.recv(10000))
+    challenge = {
+    	'private_key': 2,
+    	'host': 'www.bing.com',
+    	'curve': "secp256r1",
+    	'generator': [bait.x, bait.y]
+    }
+
+    # s.send(json.dumps(challenge).encode('utf-8'))
+    # print(s.recv(10000))
+
+
+#######################################################################################
+
+
